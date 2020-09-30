dad5a7d3660ea64dcc263df4b1115575506a879b
Libao Jin <jinlibao@outlook.com>
2020-09-29 23:00:53 -0600

    Update util.js: refactor code

--- src/util.js
+++ src/util.js
@@ -3,6 +3,14 @@ const fs = require('fs');
 const Queue = require('./Queue.js');  // http://code.iamkate.com/javascript/queues/#usingqueues
 const UnionFind = require('./UnionFind.js');
 
+function copy(object) {
+  return JSON.parse(JSON.stringify(object));
+}
+
+function print(output) {
+  console.log(JSON.stringify(output));
+}
+
 function zfill(num, places) {
   return String(num).padStart(places, '0');
 }
@@ -50,75 +58,6 @@ function isInBound(x, y, boardWidth, boardHeight) {
   return x >= 0 && x < boardWidth && y >= 0 && y < boardHeight;
 }
 
-function shortestPathToFood(boardWidth, boardHeight, head, body, food, snakes) {
-  var predecessor = initializePredecessor(boardWidth, boardHeight);
-  var board = createBoard(boardWidth, boardHeight, body, snakes);
-  var dirs = [[-1, 0], [1, 0], [0, -1], [0, 1]];
-  var queue = new Queue();
-  queue.enqueue(head);
-  var isFoodReached = false;
-  var dist = 0;
-  while (!queue.isEmpty() && !isFoodReached) {
-    var length = queue.getLength();
-    dist++;
-    for (var i = 0; i < length && !isFoodReached; ++i) {
-      var cur = queue.dequeue();
-      for (var dir of dirs) {
-        var nx = cur.x + dir[0];
-        var ny = cur.y + dir[1];
-        if (isInBound(nx, ny, boardWidth, boardHeight) && !board[nx][ny]) {
-          predecessor[nx][ny] = {x: cur.x, y: cur.y};
-          if (nx == food.x && ny == food.y) {
-            isFoodReached = true;
-            break;
-          }
-          queue.enqueue({x: nx, y: ny});
-          board[nx][ny] = true;
-        }
-      }
-    }
-  }
-  var shortestPath = new Array();
-  var cur = JSON.parse(JSON.stringify(food));
-  while (isInBound(cur.x, cur.y, boardWidth, boardHeight) && (predecessor[cur.x][cur.y].x != cur.x || predecessor[cur.x][cur.y].y != cur.y)) {
-    shortestPath.push({x: cur.x, y: cur.y});
-    var newParent = {x: predecessor[cur.x][cur.y].x, y: predecessor[cur.x][cur.y].y};
-    cur.x = newParent.x;
-    cur.y = newParent.y;
-  }
-  shortestPath.reverse();
-  return shortestPath;
-}
-
-function shortestDistanceToFood(boardWidth, boardHeight, head, body, food, snakes) {
-  var board = createBoard(boardWidth, boardHeight, body, snakes);
-  var dirs = [[-1, 0], [1, 0], [0, -1], [0, 1]];
-  var queue = new Queue();
-  queue.enqueue(head);
-  var isFoodReached = false;
-  var dist = 0;
-  while (!queue.isEmpty() && !isFoodReached) {
-    var length = queue.getLength();
-    for (var i = 0; i < length && !isFoodReached; ++i) {
-      var cur = queue.dequeue();
-      for (var dir of dirs) {
-        var nx = cur.x + dir[1];
-        var ny = cur.y + dir[0];
-        if (nx >= 0 && nx < boardWidth && ny >= 0 && ny < boardHeight && !board[nx][ny]) {
-          if (nx == food.x && ny == food.y) {
-            isFoodReached = true;
-            break;
-          }
-          queue.enqueue({x: nx, y: ny});
-          board[nx][ny] = true;
-        }
-      }
-    }
-    dist++;
-  }
-  return dist;
-}
-
 function distance(x1, y1, x2, y2) {
   return Math.abs(x1 - x2) + Math.abs(y1 - y2);
 }
@@ -134,6 +73,43 @@ function isSafe(x, y, head, body, snakes) {
     }
   }
   return true;
+}
+
+function bfs(boardWidth, boardHeight, board, from, dest) {
+  var predecessor = initializePredecessor(boardWidth, boardHeight);
+  var dirs = [[-1, 0], [1, 0], [0, -1], [0, 1]];
+  var queue = new Queue();
+  queue.enqueue(from);
+  var isDestReached = false;
+  while (!queue.isEmpty() && !isDestReached) {
+    var length = queue.getLength();
+    for (var i = 0; i < length && !isDestReached; ++i) {
+      var cur = queue.dequeue();
+      for (var dir of dirs) {
+        var nx = cur.x + dir[0];
+        var ny = cur.y + dir[1];
+        if (isInBound(nx, ny, boardWidth, boardHeight) && !board[nx][ny]) {
+          predecessor[nx][ny] = {x: cur.x, y: cur.y};
+          if (nx == dest.x && ny == dest.y) {
+            isDestReached = true;
+            break;
+          }
+          queue.enqueue({x: nx, y: ny});
+          board[nx][ny] = true;
+        }
+      }
+    }
+  }
+  var path = new Array();
+  var cur = copy(dest);
+  while (isInBound(cur.x, cur.y, boardWidth, boardHeight) && (predecessor[cur.x][cur.y].x != cur.x || predecessor[cur.x][cur.y].y != cur.y)) {
+    path.push({x: cur.x, y: cur.y});
+    var newParent = {x: predecessor[cur.x][cur.y].x, y: predecessor[cur.x][cur.y].y};
+    cur.x = newParent.x;
+    cur.y = newParent.y;
+  }
+  path.reverse();
+  return path;
 }
 
 function createUnionFindSet(boardWidth, boardHeight, body, snakes) {
@@ -166,34 +142,32 @@ function createUnionFindSet(boardWidth, boardHeight, body, snakes) {
   return result;
 }
 
-
 function determineDirection(boardWidth, boardHeight, head, body, food, snakes) {
   var result = createUnionFindSet(boardWidth, boardHeight, body, snakes);
+  var rawBoard = createBoard(boardWidth, boardHeight, body, snakes);
   var uf = result.uf;
   var cc = result.cc;
-  var shortestPaths = new Array();
+  var paths = [];
   for (var i = 0; i < food.length; ++i) {
-    var shortestPath = shortestPathToFood(boardWidth, boardHeight, head, body, food[i], snakes);
-    if (shortestPath.length > 0) {
-      shortestPaths[shortestPaths.length] = shortestPath;
-    }
+    var board = copy(rawBoard);
+    var path = bfs(boardWidth, boardHeight, board, head, food[i]);
+    paths.push(path);
   }
-  var bestPath = shortestPaths.length > 0 ? shortestPaths[0] : [];
+  var bestPath = paths.length > 0 ? paths[0] : [];
   var minDist = bestPath.length;
-  for (var i = 0; i < shortestPaths.length; ++i) {
-    if (minDist > shortestPaths[i].length) {
-      minDist = shortestPaths[i].length;
-      bestPath = shortestPaths[i];
+  for (var i = 0; i < paths.length; ++i) {
+    if (minDist > paths[i].length) {
+      minDist = paths[i].length;
+      bestPath = paths[i];
     }
   }
-
   var move = '';
   if (bestPath.length >= 1) {
     x0 = head.x;
     y0 = head.y;
     x1 = bestPath[0].x;
     y1 = bestPath[0].y;
-    if (uf.getSize(uf.find(to1D(x1, y1, boardWidth, boardHeight))) > body.length) {
+    if (uf.getSize(to1D(x1, y1, boardWidth, boardHeight)) > body.length) {
       // console.log('from (' + x0 + ', ' + y0 + ') to (' + x1 + ', ' + y1 + ')');
       if (snakes.length <= 1 || isSafe(x1, y1, head, body, snakes)) {
         if ((x0 - x1) == 0 && (y0 - y1) == 1) {
@@ -240,64 +214,59 @@ function determineDirection(boardWidth, boardHeight, head, body, food, snakes) {
   return move;
 }
 
-// test
-// const gameId = '7548c3a4-117b-460b-960e-5ad401298c70';
-// const turn = '161';
-// const gameId = 'fe3762e5-8cc0-4b5d-958b-285e45f02e36';
-// const turn = '10';
-// const root = 'test';
-// const gameId = '8a057e28-519d-4953-8663-ab1e9808bd79';
-// const turn = '42';
-// const root = '/media/libao/Files/data/battlesnake';
-// const gameId = 'e6b362ab-873c-4ae4-acd7-4bcce9212fce';
-// const turn = '40';
-// const gameData = readData(root, gameId, turn);
-// console.log(gameData);
-//
-// var dist = shortestDistanceToFood(gameData.board.width, gameData.board.height, gameData.you.head, gameData.you.body, gameData.board.food[0], gameData.board.snakes);
-// console.log(dist);
-// var shortestPath = shortestPathToFood(gameData.board.width, gameData.board.height, gameData.you.head, gameData.you.body, gameData.board.food[0], gameData.board.snakes);
-// console.log(shortestPath);
-// var move = determineDirection(gameData.board.width, gameData.board.height, gameData.you.head, gameData.you.body, gameData.board.food, gameData.board.snakes);
-// console.log(move);
-//
-// var food = gameData.board.food;
-// var boardWidth = gameData.board.width;
-// var boardHeight = gameData.board.height;
-// var x1 = 8, x2 = 8, y1 = 4, y2 = 6;
-// console.log(food);
-//
-// var result = createUnionFindSet(gameData.board.width, gameData.board.height, gameData.you.body, gameData.board.snakes);
-// var uf = result.uf;
-// var cc = result.cc;
-// console.log(`(${food[0].x}, ${food[0].y}), (${x1}, ${y1}): ${uf.connected(to1D(food[0].x, food[0].y, boardWidth, boardHeight), to1D(x1, y1, boardWidth, boardHeight))}`);
-// console.log(`(${food[0].x}, ${food[0].y}), (${x2}, ${y2}): ${uf.connected(to1D(food[0].x, food[0].y, boardWidth, boardHeight), to1D(x2, y2, boardWidth, boardHeight))}`);
-// console.log(`(${food[1].x}, ${food[1].y}), (${x1}, ${y1}): ${uf.connected(to1D(food[1].x, food[1].y, boardWidth, boardHeight), to1D(x1, y1, boardWidth, boardHeight))}`);
-// console.log(`(${food[1].x}, ${food[1].y}), (${x2}, ${y2}): ${uf.connected(to1D(food[1].x, food[1].y, boardWidth, boardHeight), to1D(x2, y2, boardWidth, boardHeight))}`);
-// console.log(uf.find(to1D(food[0].x, food[0].y, boardWidth, boardHeight)));
-// console.log(uf.find(to1D(food[1].x, food[1].y, boardWidth, boardHeight)));
-// console.log(uf.find(to1D(x1, y1, boardWidth, boardHeight)));
-// console.log(uf.find(to1D(x2, y2, boardWidth, boardHeight)));
-// console.log(uf.getSize(to1D(x1, y1, boardWidth, boardHeight)));
-// console.log(uf.getSize(to1D(x2, y2, boardWidth, boardHeight)));
-// // saveData(root, gameData);
-//
-// var s = new Set();
-// s.add(100);
-// s.add(101);
-// s.add(100);
-// console.log(s);
-// console.log(s.has(99));
-// console.log(s.has(101));
-// console.log(s);
-// console.log(cc);
-// for (let c of cc) {
-//   console.log(uf.getSize(c));
-// }
-// let sa = Array.from(s);
-// let cca = Array.from(cc);
+/*
+// test bfs and determineDirection
+const gameId      = '7548c3a4-117b-460b-960e-5ad401298c70';
+const turn        = '161';
+const gameId      = 'fe3762e5-8cc0-4b5d-958b-285e45f02e36';
+const turn        = '10';
+const root        = 'test';
+const gameId      = '8a057e28-519d-4953-8663-ab1e9808bd79';
+const turn        = '42';
+const root        = '/media/libao/Files/data/battlesnake';
+const gameId      = 'e6b362ab-873c-4ae4-acd7-4bcce9212fce';
+const turn        = '0';
+const gameData    = readData(root, gameId, turn);
+const boardWidth  = gameData.board.width;
+const boardHeight = gameData.board.height;
+const head        = gameData.you.head;
+const body        = gameData.you.body;
+const food        = gameData.board.food;
+const snakes      = gameData.board.snakes;
+const from        = head;
+const dest        = food[0];
+const board       = createBoard(boardWidth, boardHeight, body, snakes);
+print(gameData);
+
+var path = bfs(boardWidth, boardHeight, board, from, dest);
+console.log(`from: ${JSON.stringify(from)}`);
+console.log(`dest: ${JSON.stringify(dest)}`);
+console.log(`path: ${JSON.stringify(path)}`);
+
+var move = determineDirection(boardWidth, boardHeight, head, body, food, snakes);
+console.log(`move: ${move}`);
+
+// test UnionFind
+var x1 = 8, x2 = 8, y1 = 4, y2 = 6;
+console.log(food);
+
+var result = createUnionFindSet(boardWidth, boardHeight, body, snakes);
+var uf = result.uf;
+var cc = result.cc;
+console.log(`(${food[0].x}, ${food[0].y}), (${x1}, ${y1}): ${uf.connected(to1D(food[0].x, food[0].y, boardWidth, boardHeight), to1D(x1, y1, boardWidth, boardHeight))}`);
+console.log(`(${food[0].x}, ${food[0].y}), (${x2}, ${y2}): ${uf.connected(to1D(food[0].x, food[0].y, boardWidth, boardHeight), to1D(x2, y2, boardWidth, boardHeight))}`);
+console.log(`(${food[1].x}, ${food[1].y}), (${x1}, ${y1}): ${uf.connected(to1D(food[1].x, food[1].y, boardWidth, boardHeight), to1D(x1, y1, boardWidth, boardHeight))}`);
+console.log(`(${food[1].x}, ${food[1].y}), (${x2}, ${y2}): ${uf.connected(to1D(food[1].x, food[1].y, boardWidth, boardHeight), to1D(x2, y2, boardWidth, boardHeight))}`);
+console.log(uf.find(to1D(food[0].x, food[0].y, boardWidth, boardHeight)));
+console.log(uf.find(to1D(food[1].x, food[1].y, boardWidth, boardHeight)));
+console.log(uf.find(to1D(x1, y1, boardWidth, boardHeight)));
+console.log(uf.find(to1D(x2, y2, boardWidth, boardHeight)));
+console.log(uf.getSize(to1D(x1, y1, boardWidth, boardHeight)));
+console.log(uf.getSize(to1D(x2, y2, boardWidth, boardHeight)));
+// saveData(root, gameData);
+*/
 
 module.exports = {
   zfill, saveData, readData, createBoard, initializePredecessor,
-  shortestPathToFood, shortestDistanceToFood, determineDirection
+  bfs, determineDirection
 };
